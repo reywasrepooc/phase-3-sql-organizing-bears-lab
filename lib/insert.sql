d069775c7547423024635d093a57bfc3deaf3d26
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-17 14:53:16 -0400

    commit tree f3a02383f7eae088680251cff3470353b3f5bd8b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'Brown', 'heated', 1); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Purple','calm', 1 ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'BLACK', 'AGGRESSIVE', 1); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'F', 'GREEN', 'chill', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Yellow', 'relaxed', 1 ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Red', 'mellow', 1); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'M', 'White', 'aggro', 1); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( NULL, 12, 'M', 'Orange', 'Dismissive', 1); 
